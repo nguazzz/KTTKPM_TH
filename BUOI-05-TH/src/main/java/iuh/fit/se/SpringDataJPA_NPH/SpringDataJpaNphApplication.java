8717f3df8f21591a5d8f78a4424ab1845e217189
nguazzz <kuhoangkt@gmail.com>
2022-10-02 14:45:06 +0700

    TH5

---
+++ BUOI-05-TH/src/main/java/iuh/fit/se/SpringDataJPA_NPH/SpringDataJpaNphApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.SpringDataJPA_NPH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaNphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaNphApplication.class, args);
+	}
+
+}
---
+++ BUOI-05-TH/src/main/java/iuh/fit/se/SpringDataJPA_NPH/controller/ChuyenBayController.java
@@ -0,0 +1,10 @@
+package iuh.fit.se.SpringDataJPA_NPH.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("./chuyenbay")
+public class ChuyenBayController {
+	
+}
